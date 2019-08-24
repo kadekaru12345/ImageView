ab495c7217ffb0de4a13efba449f67c6c8acdda8
kadekaru12345 <kadekaru12345@gmail.com>
2019-08-24 21:09:43 +0900

    ImageView(テキスト4-3-7)

---
+++ app/src/main/java/com/shouexample/k/imageview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shouexample.k.imageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
